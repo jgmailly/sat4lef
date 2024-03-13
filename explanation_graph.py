17b57320a3af2cdaef617465ab27a8b6d3e5ddd0
jgmailly <jeanguy.mailly@gmail.com>
2024-03-13 16:07:00 +0100

    Work in progress: activation of the explanation graph

--- explanation_graph.py
+++ explanation_graph.py
@@ -1,3 +1,5 @@
+from graphviz import Digraph
+
 class ClauseNode:
     def __init__(self, node_type):
         self.node_type = node_type
@@ -75,7 +77,7 @@ class ExplanationGraph:
         clause_nodes = []
         for node in self.nodes:
             if node.get_node_type() in ["at-least-one-object-per-agent", "at-most-one-agent-per-object", "lef-clause"]:
-                clause_node.append(node)
+                clause_nodes.append(node)
         return clause_nodes
         
     def get_predecessors(self, node):
@@ -100,19 +102,6 @@ class ExplanationGraph:
             str_result += f"edge({edge[0].to_string()},{edge[1].to_string()}).\n"
         return str_result
 
-    def to_dot_basic(self):
-        str_result = "digraph G {\n"
-        for node in self.nodes:
-            str_result += node.content() + " "
-            if node.get_node_type() in ["top", "bottom", "var"]:
-                str_result += "[shape=box];\n"
-            else:
-                str_result += "[shape=ellipse];\n"
-        for edge in self.edges:
-            str_result += edge[0].content() + " -> " + edge[1].content() + " ;\n"
-        str_result += "}"
-        return str_result
-
     def to_dot(self):
         str_result = "digraph G {\n"
         for node in self.nodes:
@@ -125,6 +114,48 @@ class ExplanationGraph:
             str_result += "n" + str(edge[0].get_node_id_in_graph(self)) + " -> " + "n" + str(edge[1].get_node_id_in_graph(self)) + " ;\n"
         str_result += "}"
         return str_result
+
+    def to_dot_with_activation(self, activation):
+        str_result = "digraph G {\n"
+        for node in self.nodes:
+            index = node.get_node_id_in_graph(self)
+            str_result += "n" + str(node.get_node_id_in_graph(self)) + " "
+            if node.get_node_type() in ["top", "bottom", "var"]:
+                str_result += f"[shape=ellipse, label=\"{node.content()}\""
+            else:
+                str_result += f"[shape=box, label=\"{node.content()}\""
+
+            if activation[index]:
+                str_result += ", style=filled, fillcolor=green"
+
+            str_result += "];\n"
+        for edge in self.edges:
+            str_result += "n" + str(edge[0].get_node_id_in_graph(self)) + " -> " + "n" + str(edge[1].get_node_id_in_graph(self)) + " ;\n"
+        str_result += "}"
+        return str_result
+
+    def to_graphviz(self, activation, act_index):
+        graph = Digraph()
+
+        for node in self.nodes:
+            index = node.get_node_id_in_graph(self)
+            node_name = "n" + str(node.get_node_id_in_graph(self))
+
+            if node.get_node_type() in ["top", "bottom", "var"]:
+                if activation[index]:
+                    graph.node(name=node_name, shape="ellipse", label=f"{node.content()}", style="filled", fillcolor="green")
+                else:
+                    graph.node(name=node_name, shape="ellipse", label=f"{node.content()}")
+            else:
+                if activation[index]:
+                    graph.node(name=node_name, shape="box", label=f"{node.content()}", style="filled", fillcolor="green")
+                else:
+                    graph.node(name=node_name, shape="box", label=f"{node.content()}")
+
+        for edge in self.edges:
+            graph.edge("n" + str(edge[0].get_node_id_in_graph(self)), "n" + str(edge[1].get_node_id_in_graph(self)))
+
+        graph.render(filename=f"Activation-{str(act_index).zfill(3)}", cleanup=True, view=False, format='png')
 
     def get_successor_activations(self, activation):
         successors = []
@@ -140,15 +171,21 @@ class ExplanationGraph:
                     if not activation[pred_index]:
                         all_pred_activated = False
                 if all_pred_activated:
-                    successor = True
+                    successor[index] = True
+                    break
+                    
 
-        ####### TO DO
-        # Third item of the definition of v^{t+1}
-        # ''If x is a clause-node and becomes activated in v^t, then one of its successor variable-node
-        # y is chosen to be activated in v^{t+1}''
-        # This is where the 'non-determinism' is added
-            
-        return None
+        print(f"Activation of clause node {index}: {self.nodes[index].content()}")
+
+        successor_nodes = self.get_successors(self.nodes[index])
+        for successor_node in successor_nodes:
+            successor_index = successor_node.get_node_id_in_graph(self)
+            new_successor = successor.copy()
+            new_successor[successor_index] = True
+            successors.append(new_successor)
+            print(f"Activation of var node {successor_index}: {self.nodes[successor_index].content()}")
+        
+        return successors
     
     def activate(self):
         new_activation = []
@@ -157,27 +194,28 @@ class ExplanationGraph:
                 new_activation.append(True)
             else:
                 new_activation.append(False)
-        self.activations.append(new_activation)
+#        self.activations.append(new_activation)
             
-        next_activations = [self.activations[0]]
+        next_activations = [new_activation]
         while next_activations != []:
             next_activation = next_activations.pop(0)
             if next_activation not in self.activations:
                 self.activations.append(next_activation)
-                successor_activations = get_successor_activations(self, next_activation)
+                successor_activations = self.get_successor_activations(next_activation)
                 next_activations = successor_activations + next_activations
 
+        act_index = 0
         for activation in self.activations:
+            self.to_graphviz(activation, act_index)
+
+            print("-------------")
+            print(f"Activation-{str(act_index).zfill(3)}")
             print(activation)
+            for index in range(len(activation)):
+                if activation[index]:
+                    print(f"index: {index} - node: {self.nodes[index].content()}")
+            #print(self.to_dot_with_activation(activation))
+            print("-------------\n")
 
+            act_index += 1
 
-
-if __name__ == "__main__":
-    graph = ExplanationGraph()
-    top = ClauseNode("top")
-    graph.add_node(top)
-    bottom = ClauseNode("bottom")
-    graph.add_node(bottom)
-    graph.add_edge(top, bottom)
-    print(graph.to_string())
-    
