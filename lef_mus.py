f5581fb48b7f819b6c2dce3ef109de13d18b5a9e
Anaëlle Wilczynski <anaelle.wilczynski@centralesupelec.fr>
2024-04-04 08:57:07 +0200

    generation of random instances + mus counting for basic and redundant formulation

---
+++ lef_mus.py
@@ -0,0 +1,259 @@
+import sys
+import argparse
+import os
+from utils import *
+from random_instances import *
+
+from pysat.solvers import Solver
+from pysat.examples.rc2 import RC2
+from pysat.examples.musx import MUSX
+from pysat.examples.optux import OptUx
+from pysat.examples.mcsls import MCSls
+from pysat.formula import WCNF
+
+
+
+class lef_mus:
+    def __init__(self, location):
+        self.soc_file = open(location + ".soc",'r')
+        self.pref_file = open(location + ".pref",'r')
+        self.preferences = dict()
+        self.agents = []
+        self.pref_parser()
+        self.agents, self.social, self.number_of_edges = parse_tgf(self.soc_file)
+        self.items = get_objects_from_preferences(self.agents,self.preferences)
+        self.SAT_variables_meaning = dict()
+        self.get_variable_meaning()
+        self.redundant = False
+        self.clauses = []
+        self.minimum_muses = []
+        self.all_muses = []
+        self.pref_file.close()
+        self.soc_file.close()
+    
+
+    def pref_parser(self):
+        preferences_lines = self.pref_file.read().splitlines()
+        for pref_line in preferences_lines:
+            agent, pref_list = parse_preference_line(pref_line)
+            self.preferences[agent] = pref_list
+
+    def get_variable_meaning(self):
+        for agent in self.agents:
+            for item in self.items:
+                #print(f"alloc({agent},{item}) = {get_SAT_variable(agent, agents, item, items)}")
+                self.SAT_variables_meaning[get_SAT_variable(agent, self.agents, item, self.items)] = f"alloc({agent},{item})"
+
+    def get_agents(self):
+        return self.agents
+
+    def get_items(self):
+        return self.items
+
+    def get_preferences(self):
+        return self.preferences    
+
+    def get_clauses(self):
+        return self.clauses    
+    
+    def get_nb_clauses(self):
+        return len(self.clauses)
+    
+    def get_minimum_muses(self):
+        return self.minimum_muses
+    
+    def get_all_muses(self):
+        return self.all_muses
+
+    def sat_encoding(self, redundant):
+        if len(self.clauses) > 0: 
+            if self.redundant == redundant:
+                return
+            else:
+                self.clauses.clear()
+                self.minimum_muses.clear()
+                self.all_muses.clear()
+        self.redundant = redundant
+        for agent in self.agents:
+            self.clauses.append(at_least_one_item(agent, self.agents, self.items)) # clause \phi_{alloc}^{\geq 1}(i)
+            other_agents = [other_agent for other_agent in self.agents if other_agent > agent]
+            for item in self.items:
+                for other_agent in other_agents:
+                    self.clauses.append(agents_do_not_share_items(agent, other_agent, self.agents, item, self.items)) # clause \phi_{alloc}^O(o,i,j)
+        
+        if redundant:
+            for item in self.items:
+                self.clauses.append(at_least_one_agent(item, self.agents, self.items))
+                other_items = [other_item for other_item in self.items if other_item > item]
+                for agent in self.agents:
+                    for other_item in other_items:
+                        self.clauses.append(items_do_not_share_agents(agent, self.agents, item, other_item, self.items)) # clause \phi_{alloc}^O(o,i,j)
+
+            
+        for edge in self.social:
+            for item in self.items:
+                clause = [-get_SAT_variable(edge[0], self.agents, item, self.items)]
+
+                other_items = self.items.copy()
+                other_items.remove(item)
+                for other_item in other_items:
+                    if agent_prefers(self.preferences, edge[1], other_item, item) and agent_prefers(self.preferences, edge[0], item, other_item):
+                        clause.append(get_SAT_variable(edge[1], self.agents, other_item, self.items))
+
+                #print(f"--- phi_LEF({edge[0]},{edge[1]},{item}) = {clause} = {clause_as_text(clause,SAT_variables_meaning)}")
+                self.clauses.append(clause) # clause \phi_{lef}(i,j,o)
+        
+                # reverse direction of the edge
+                clause = [-get_SAT_variable(edge[1], self.agents, item, self.items)]
+
+                for other_item in other_items:
+                    if agent_prefers(self.preferences, edge[0], other_item, item) and agent_prefers(self.preferences, edge[1], item, other_item):
+                        clause.append(get_SAT_variable(edge[0], self.agents, other_item, self.items))
+
+                #print(f"--- phi_LEF({edge[1]},{edge[0]},{item}) = {clause} = {clause_as_text(clause,SAT_variables_meaning)}")
+                self.clauses.append(clause) # clause \phi_{lef}(i,j,o)
+
+
+
+    def compute_mus(self,mus,enum):
+        if len(self.clauses) == 0 or len(self.minimum_muses) > 0:
+            return True, None
+        s = Solver(name='g4', bootstrap_with=self.clauses)
+        if s.solve():
+            model = s.get_model()
+            s.delete()
+            return True, decode_model_into_alloc(model,self.SAT_variables_meaning)      
+        elif mus:
+            s.delete()
+            cnf = WCNF()
+            for clause in self.clauses:
+                cnf.append(clause, weight=1)
+            #print(cnf)
+            #musx = MUSX(cnf,verbosity=0)
+            with OptUx(cnf) as optux:
+                MUS = optux.compute()
+            if MUS == None:
+                return True, None 
+            else:
+                res = []
+                for index in MUS:
+                    res.append(clause_as_text(self.clauses[index-1],self.SAT_variables_meaning))
+            if enum: 
+                nb_mus=0
+                min_cost=0
+                min = True
+                with OptUx(cnf) as optux:
+                    for mus in optux.enumerate():
+                        if enum and min_cost !=0 and min_cost!=optux.cost:
+                            min = False
+                        nb_mus+=1
+                        min_cost = optux.cost
+                        if (min):
+                            self.minimum_muses.append((mus,optux.cost))
+                        self.all_muses.append((mus,optux.cost))
+            return False, res #clause_as_text(self.clauses[self.minimum_muses[0][0]-1],self.SAT_variables_meaning)
+
+    
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("nb_agents", type=int, help="specify the number of agents")
+    parser.add_argument("sample_size", type=int, help="specify the number of generated instances")
+    parser.add_argument("model", help="specify the statistical model for graph generation")
+    parser.add_argument("parameter", type=float, help="specify the statistical model for graph generation")
+    args = parser.parse_args()
+    n_args = len(sys.argv)
+    path = os.getcwd()
+    nb_false_instances = 0
+
+    nb_clauses = 0
+    nb_clauses_redundant = 0
+
+    nb_mus = 0
+    nb_min_mus = 0
+    min_mus_size = 0
+    max_mus_size = 0
+
+    nb_mus_redundant = 0
+    nb_min_mus_redundant = 0
+    min_mus_size_redundant = 0
+    max_mus_size_redundant = 0
+    for i in range(args.sample_size):
+        print("\ncounter: "+str(i+1))
+        filename = args.model+"/test_par"+str(args.parameter)+"_"+str(args.nb_agents)+"ag_"+str(i+1)
+        location = os.path.join(path, "tests/random/"+filename)
+        if (not os.path.exists(location+".soc")):
+            graph = generate_graph(args.model,args.nb_agents,n_args)
+            print_social_network(graph,location,filename)
+        if (not os.path.exists(location+".pref")):
+            print_preferences(location,args.nb_agents)
+        encoding = lef_mus(location)
+        encoding.sat_encoding(False)
+        #print("number of clauses: "+str(encoding.get_nb_clauses()))
+        nb_clauses += encoding.get_nb_clauses()
+        #print(encoding.compute_mus(True,False)) 
+        if not encoding.compute_mus(True,True)[0]:
+            nb_false_instances += 1
+            nb_mus += len(encoding.get_all_muses())
+            nb_min_mus += len(encoding.get_minimum_muses())
+            if (len(encoding.get_minimum_muses()) > 0):
+                min_mus_size += encoding.get_minimum_muses()[0][1]
+                max_mus_size += encoding.get_all_muses()[-1][1]     
+        #if (len(encoding.get_minimum_muses()) > 0):
+            #print(encoding.get_minimum_muses())
+            #print("number of minimum MUSes: "+str(len(encoding.get_minimum_muses())))
+            #print("total number of MUSes: "+str(len(encoding.get_all_muses())))
+            #print("minimum size of a MUS: "+str(encoding.get_minimum_muses()[0][1]))
+            #print("maximum size of a MUS: "+str(encoding.get_all_muses()[-1][1]))
+        encoding.sat_encoding(True)
+        #print("number of clauses: "+str(encoding.get_nb_clauses()))
+        nb_clauses_redundant += encoding.get_nb_clauses()
+        #print(encoding.compute_mus(True,False))   
+        if not encoding.compute_mus(True,True)[0]:
+            nb_mus_redundant += len(encoding.get_all_muses())
+            nb_min_mus_redundant += len(encoding.get_minimum_muses())
+            if (len(encoding.get_minimum_muses()) > 0):
+                min_mus_size_redundant += encoding.get_minimum_muses()[0][1]
+                max_mus_size_redundant += encoding.get_all_muses()[-1][1]      
+        #if (len(encoding.get_minimum_muses()) > 0):
+            #print(encoding.get_minimum_muses())
+            #print("number of minimum MUSes: "+str(len(encoding.get_minimum_muses())))
+            #print("total number of MUSes: "+str(len(encoding.get_all_muses())))
+            #print("minimum size of a MUS: "+str(encoding.get_minimum_muses()[0][1]))
+            #print("maximum size of a MUS: "+str(encoding.get_all_muses()[-1][1]))
+        #print(redundant_encoding.get_nb_clauses())
+        #print(redundant_encoding.get_mus(False,False))
+                
+    output_name = "results_"+str(args.model)+str(args.parameter)
+    output_location = os.path.join(path, "tests/random/"+output_name)
+    output_file = open(output_location + ".txt",'a')
+    output_file.write("\n\n===================================")
+    output_file.write("\nn = "+str(args.nb_agents))
+    output_file.write("\nsample size = "+str(args.sample_size))
+    output_file.write("\nnb false instances = "+str(nb_false_instances))
+    output_file.write("\n===================================")
+    output_file.write("\n----------BASIC ENCODING----------")
+    output_file.write("\naverage number of clauses: "+str(nb_clauses / args.sample_size))
+    output_file.write("\naverage number of minimum MUSes: "+str(nb_min_mus / nb_false_instances))
+    output_file.write("\naverage total number of MUSes: "+str(nb_mus / nb_false_instances))
+    output_file.write("\naverage minimum size of a MUS: "+str(min_mus_size / nb_false_instances))
+    output_file.write("\naverage maximum size of a MUS: "+str(max_mus_size / nb_false_instances))
+    output_file.write("\n----------REDUNDANT ENCODING----------")
+    output_file.write("\naverage number of clauses: "+str(nb_clauses_redundant / args.sample_size))
+    output_file.write("\naverage number of minimum MUSes: "+str(nb_min_mus_redundant / nb_false_instances))
+    output_file.write("\naverage total number of MUSes: "+str(nb_mus_redundant / nb_false_instances))
+    output_file.write("\naverage minimum size of a MUS: "+str(min_mus_size_redundant / nb_false_instances))
+    output_file.write("\naverage maximum size of a MUS: "+str(max_mus_size_redundant / nb_false_instances))
+    output_file.close()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ random_instances.py
@@ -0,0 +1,76 @@
+import sys
+import random
+import networkx as nx
+import os
+import argparse
+
+
+
+def print_social_network(graph,location,filename):    
+    soc_file = open(location + ".soc",'w')
+    for node in graph.nodes():
+        print("A"+str(node+1),file = soc_file)
+    print("#",file = soc_file)
+    for edge in graph.edges():
+        print(f"{"A"+str(edge[0]+1)} {"A"+str(edge[1]+1)}",file = soc_file)
+    soc_file.close()
+
+def usage():
+    sys.exit("Usage: python3 random_instances.py nb_agents sample_size model [params...] where\n\tnb_agents is the number of agents/objects\n\tsample_size is the number of generated instances\n\tmodel is the generation model: er for Erdos-Renyi, line for lines, ba for Barabasi-Albert, ws for Watts-Strogatz\n\tparams are optional parameters, e.g. the probability for ER, the attachment value for BA, the number of joint nodes and the probability of rewiring for WS")
+
+
+def print_preferences(location,n):
+    pref_file = open(location + ".pref", 'w')
+    objects = ["o" + str(i) for i in range(1,n+1)]
+    for i in range(n):
+        objects_bis = objects.copy()
+        random.shuffle(objects_bis)
+        pref_string = "A"+str(i+1)+":"+objects_bis.pop()
+        for obj in objects_bis:
+            pref_string += " "+obj
+        print(pref_string, file = pref_file)
+    pref_file.close()
+
+
+def generate_graph(model,n,n_args):
+    graph = None
+    if model == "er":
+        if n_args < 5:
+            usage()
+        proba = float(sys.argv[4])
+        graph = nx.erdos_renyi_graph(n,proba)
+    elif model == "LINE":
+        graph = nx.path_graph(n)
+    elif model == "ba":
+        if n_args < 5:
+            usage()
+        attachment = int(sys.argv[4])
+        graph=nx.barabasi_albert_graph(n,attachment)
+    elif model == "ws":
+        if n_args < 6:
+            usage()
+        joint = int(sys.argv[4])
+        rewiring = float(sys.argv[5])
+        graph = nx.watts_strogatz_graph(n,joint,rewiring)
+    return graph
+
+def main(): 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("nb_agents", type=int, help="specify the number of agents")
+    parser.add_argument("sample_size", type=int, help="specify the number of generated instances")
+    parser.add_argument("model", help="specify the statistical model for graph generation")
+    parser.add_argument("parameter", type=float, help="specify the statistical model for graph generation")
+    args = parser.parse_args()
+    n_args = len(sys.argv)
+    path = os.getcwd()
+    for i in range(args.sample_size):
+        filename = args.model+"/test_par"+str(args.parameter)+"_"+str(args.nb_agents)+"ag_"+str(i+1)
+        graph = generate_graph(args.model,args.nb_agents,n_args)
+        location = os.path.join(path, "tests/random/"+filename)
+        print_social_network(graph,location,filename)
+        print_preferences(location,args.nb_agents)
+
+
+if __name__ == "__main__":
+    main()
+
